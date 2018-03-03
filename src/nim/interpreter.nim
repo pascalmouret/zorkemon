6f230a764e97864a52fefa5e5f961c8b4c07ec7a
Pascal Mouret <pascal.mouret@avrios.com>
2018-03-04 00:33:50 +0100

    add primitive option

--- src/nim/interpreter.nim
+++ src/nim/interpreter.nim
@@ -1,12 +1,35 @@
 from strutils import splitWhitespace
+import lib.option
 
-type Interpreter* = ref object of RootObj
+type
+  Word* = ref object
+    value: string
 
-method parse*(interpreter: Interpreter, input: string): string {.base.} =
-  result = ""
+  Interpreter* = ref object of RootObj
+    words: seq[Word]
+
+proc newInterpreter(words: seq[Word]): Interpreter =
+  Interpreter(words: words)
+
+method wordForToken(self: Interpreter, token: string): Option[Word] {.base.} =
+  for word in self.words:
+    if word.value == token:
+      return Some(word)
+  return None[Word]()
+
+method parse*(self: Interpreter, input: string): string {.base.} =
+  var col: seq[Word] = @[]
   for token in input.splitWhitespace():
-    result = result & token
+    let opt = self.wordForToken(token)
+    if not opt.isEmpty:
+      col.add(opt.get)
+  result = ""
+  for word in col:
+    result = result & word.value
+
 
 when isMainModule:
-  var intpr = Interpreter()
-  echo intpr.parse("test input")
+  let intpr = Interpreter(words: @[Word(value: "go"), Word(value: "north")])
+  assert(intpr.parse("test input") == "")
+  assert(intpr.parse("go north") == "gonorth")
+  assert(intpr.parse("go west") == "go")
